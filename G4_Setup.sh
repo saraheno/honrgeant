9bfdbc0e14381001149ab87e456c33ec45d600b6
Sarah <eno@umd.edu>
2015-07-20 15:37:46 -0400

    ha

--- G4_Setup.sh
+++ G4_Setup.sh
@@ -5,6 +5,9 @@ echo $MYLOCATION
 set path = ($path /data/users/eno/cmake-3.3.0-rc3/bin)
 set path = ($path /data/users/eno/geant4.10.01.p02-install/share/Geant4-10.1.2/data)
 
+cd /home/eno/CMSSW_5_3_3
+cmsenv
+
 set MYGEANT = /data/users/eno/geant4.10.01.p02-install
 set MYGEANT2 = $MYGEANT/lib64/Geant4-10.1.2
 set MYGEANT3 = /data/users/eno/geant4.10.01.p02
