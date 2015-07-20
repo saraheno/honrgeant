caa6c87d497b27972cab09ed08517c096368174c
Sarah <eno@umd.edu>
2015-07-20 13:26:55 -0400

    recover lost file

---
+++ G4_Setup.sh
@@ -0,0 +1,16 @@
+#!/bin/csh
+set MYLOCATION = `pwd`
+echo $MYLOCATION
+
+set path = ($path /data/users/eno/cmake-3.3.0-rc3/bin)
+set path = ($path /data/users/eno/geant4.10.01.p02-install/share/Geant4-10.1.2/data)
+
+set MYGEANT = /data/users/eno/geant4.10.01.p02-install
+set MYGEANT2 = $MYGEANT/lib64/Geant4-10.1.2
+set MYGEANT3 = /data/users/eno/geant4.10.01.p02
+
+cd $MYGEANT/bin
+pwd
+csh geant4.csh
+cd $MYLOCATION
+
